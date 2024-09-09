1aa8dae341eb7ef375cf2ed1fc727ebdaf5f6dea
IsaacS2Hud <sanchezisaac809@gmail.com>
2024-09-09 17:15:14 -0500

    Fixes animation bugs and adds arrow movement
    The girl model now stays in the center of the gameobject, not editing the positioning or rotation of the hips rig object or the box collider. Up/down/left/right movement has also been added. Some camera rotation should be added.

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class movement : MonoBehaviour
 {
-    [SerializeField] private float flightSpeed;
-    [SerializeField] private float breathValue;
+    [SerializeField] private float flightSpeed, turnSpeed, maxHorizontalTurn, maxVerticalTurn, rotateTime, breathValue;
+    [SerializeField] private InputActionReference turning, breathing;
 
+    private Vector2 turnDirection;
+    private float currentRotation;
     private Rigidbody rb;
 
-    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        
+    }
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,11 +25,16 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.rotation = Quaternion.Slerp(currentRotation, , rotateTime);
+        //timeCount = timeCount + Time.deltaTime;
+
+        turnDirection = turning.action.ReadValue<Vector2>() * turnSpeed;
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = transform.forward * flightSpeed;
+        rb.velocity = (transform.forward * flightSpeed) + new Vector3(turnDirection.x, turnDirection.y, 0);
     }
+
+
 }
