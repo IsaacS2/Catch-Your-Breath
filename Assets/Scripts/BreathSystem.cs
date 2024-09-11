6ec428168031c7995a4a76e3f735c4e17627992b
IsaacS2Hud <sanchezisaac809@gmail.com>
2024-09-11 16:17:55 -0500

    Refines UI for breathing system
    UI now consists of a more prominent ring with a circle indicating current breath stage value. The UI will disappear when the player is not maintaining breathing.

--- Assets/Scripts/BreathSystem.cs
+++ Assets/Scripts/BreathSystem.cs
@@ -1,12 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
 public class BreathSystem : MonoBehaviour
 {
-    [SerializeField] private InputActionReference turning, breathIn, breathOut;
-    private int breathingState;
+    [SerializeField] private InputActionReference breathIn, breathOut;
+    [SerializeField] private float maxBreathlessTime, segmentPercents, breathAccuracySegments = 3;
+    [SerializeField] private Image breathRing;
+
+    public event Action<float> OnBreathComplete = (_breathMultiplier) => { };
+    private Vector2 breathStartSize;
+    private float breathlessTimer, breathStageTimer;
+    private BreathStates breathingState;
 
     private void OnEnable()
     {
@@ -16,14 +24,41 @@ public class BreathSystem : MonoBehaviour
         breathOut.action.performed += StopBreath;
     }
 
+    private void Start()
+    {
+        breathingState = BreathStates.Idle;  // player starts out not breathing
+        RectTransform initialBreathRect = (RectTransform)breathRing.transform;
+        breathStartSize = initialBreathRect.sizeDelta;
+        Debug.Log(breathStartSize);
+    }
+
+    private void Update()
+    {
+        if (breathingState != BreathStates.Idle)
+        {
+            breathStageTimer += Time.deltaTime;
+        }
+        else
+        {
+            breathlessTimer += Time.deltaTime;
+        }
+    }
+
     private void BreathIn(InputAction.CallbackContext obj)
     {
-
+        if (breathingState == BreathStates.Idle)
+        {
+            breathingState = BreathStates.BreathingIn;
+        }
     }
 
     private void HoldBreath(InputAction.CallbackContext obj)
     {
+        if (breathingState == BreathStates.BreathingIn)
+        {
+            breathingState = BreathStates.BreathingIn;
 
+        }
     }
 
     private void BreathOut(InputAction.CallbackContext obj)
--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -5,38 +5,44 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float flightSpeed, turnSpeed, maxHorizontalTurn, maxVerticalTurn, rotateTime;
+    [SerializeField] private float flightSpeed, originalTurnSpeed;
     [SerializeField] private InputActionReference turning;
+    [SerializeField] private BreathSystem lungs;
 
-    private Vector3 startRotation, currentRotation;
+    private Vector3 startRotation;
     private Vector2 turnDirection;
-    private float breathMultiple;
+    private float currentTurnSpeed;
     private Rigidbody rb;
 
     private void OnEnable()
     {
+        lungs.OnBreathComplete += IncreaseDirectionalSpeed;
         startRotation = transform.eulerAngles;
+        currentTurnSpeed = originalTurnSpeed;
     }
 
     void Start()
     {
-        breathMultiple = 1;
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        turnDirection = turning.action.ReadValue<Vector2>() * turnSpeed;
-
-        //Vector3 maxRotation = new Vector3(maxHorizontalTurn * turnDirection.x, maxVerticalTurn * turnDirection.y, 0);
-        //Vector3 newRotation = Vector3.Lerp(currentRotation, maxRotation, Time.deltaTime);
+        turnDirection = turning.action.ReadValue<Vector2>() * currentTurnSpeed;
     }
 
     private void FixedUpdate()
     {
+        //
+        // moves player in local xy plane
+        //
         rb.velocity = (transform.forward * flightSpeed) 
-            + (turnDirection.x * transform.right * breathMultiple) 
-            + (turnDirection.y * transform.up * breathMultiple);
+            + (turnDirection.x * transform.right) 
+            + (turnDirection.y * transform.up);
+    }
+
+    private void IncreaseDirectionalSpeed(float _breathMultiplier)
+    {
+        currentTurnSpeed = originalTurnSpeed * _breathMultiplier;
     }
 }
