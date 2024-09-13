d6b03c4a3606817bbdfed9eb2b9dc18dcd395d80
IsaacS2Hud <sanchezisaac809@gmail.com>
2024-09-12 21:06:00 -0500

    Adds GameManager, checkpoint, and wake up scripts
    Scripts have not been implemented yet, besides GameManager.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private GameObject player;
+    private Vector3 currentCheckpointPosition;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(Instance);
+        }
+
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            player.transform.position = currentCheckpointPosition != Vector3.zero ? currentCheckpointPosition : player.transform.position;
+        }
+    }
+
+    public void setCheckpoint(Vector3 _newCheckpoint)
+    {
+        currentCheckpointPosition = _newCheckpoint;
+    }
+}
